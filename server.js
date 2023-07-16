164c3e76a43c1a9dbbad9e59aea9ac2b52889b70
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-16 19:14:49 -0300

    changed index.html in variable to notes.html and installed uuid

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/index.html');
+const api = require('./public/notes.html');
+const {v4: uuid} = require('uuid');
 
 const PORT = 3001;
 
