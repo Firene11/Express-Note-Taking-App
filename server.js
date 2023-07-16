43bc6852d87192bf292ff7805ddf308811f61795
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-16 20:27:57 -0300

    commented lines 3 and 13 out, they are making the server break

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/notes.html');
+//const api = require('./public/notes.html');
 const {v4: uuid} = require('uuid');
 
 const PORT = 3001;
@@ -10,7 +10,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+//app.use('/api', api);
 
 app.use(express.static('public'));
 
