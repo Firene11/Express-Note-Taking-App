6b39c7fc37ffc83945b832176f4466f3f67d14f5
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-17 14:35:00 -0300

    removed unwanted duplicate code

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
+app.use('/api', api);
 
 //GET Route for homepage
 app.get('/', (req, res) =>
@@ -24,10 +25,6 @@ res.sendFile(path.join(__dirname, '/public/index.html'))
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} `)
