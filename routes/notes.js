d1fb868919877868edb06f90a35cd2abf9185309
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-16 21:31:38 -0300

    added exports and middlewear

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+const tips = require('express').Router();
+const uuid = require('../helpers/uuid');
+
+// Helper functions for reading and writing to the JSON file
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+// This API route is a GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// This API route is a POST Route for a new note
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { username, title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      username,
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully `);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
